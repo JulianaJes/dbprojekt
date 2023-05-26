a739fea89a263c457fbdaf5b3bb340fa4e6aae00
KaiserVonChina <131445235+KaiserV0nChina@users.noreply.github.com>
2023-05-26 08:21:45 +0200

    Update

--- Urlaubsplanung/src/urlaubsplanung/Reiseanbieter.java
+++ Urlaubsplanung/src/urlaubsplanung/Reiseanbieter.java
@@ -11,4 +11,55 @@ package urlaubsplanung;
  */
 public class Reiseanbieter {
     public int RaID;
+    public String Name;
+    public int TelNr;
+    public String EMail;
+
+    public Reiseanbieter() {
+    }
+
+    public Reiseanbieter(int RaID, String Name, int TelNr, String EMail) {
+        this.RaID = RaID;
+        this.Name = Name;
+        this.TelNr = TelNr;
+        this.EMail = EMail;
+    }
+
+    public int getRaID() {
+        return RaID;
+    }
+
+    public void setRaID(int RaID) {
+        this.RaID = RaID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getTelNr() {
+        return TelNr;
+    }
+
+    public void setTelNr(int TelNr) {
+        this.TelNr = TelNr;
+    }
+
+    public String getEMail() {
+        return EMail;
+    }
+
+    public void setEMail(String EMail) {
+        this.EMail = EMail;
+    }
+
+    @Override
+    public String toString() {
+        return "Reiseanbieter{" + "RaID=" + RaID + ", Name=" + Name + ", TelNr=" + TelNr + ", EMail=" + EMail + '}';
+    }
+    
 }
